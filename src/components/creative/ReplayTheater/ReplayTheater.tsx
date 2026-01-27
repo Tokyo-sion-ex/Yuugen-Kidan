31ec9aa64981cf62efe7036af13cf90af9dc4a05
東京紫苑 <11123-033@gs.machida-tky.ed.jp>
2026-01-27 11:37:41 +0900

    Create ReplayTheater.tsx

---
+++ src/components/creative/ReplayTheater/ReplayTheater.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ReplayTheater: React.FC = () => {
+  return (
+    <div className="replay-theater">
+      <h2>🎬 リプレイシアター</h2>
+      <p>この機能は現在開発中です。</p>
+      <p>ドラマチックなリプレイ再生と解説システムが含まれます。</p>
+    </div>
+  );
+};
