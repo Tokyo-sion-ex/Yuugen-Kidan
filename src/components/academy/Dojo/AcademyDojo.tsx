52788f2162fa67945bf3a361370b83bf5e8280cf
東京紫苑 <11123-033@gs.machida-tky.ed.jp>
2026-01-27 11:38:30 +0900

    Create AcademyDojo.tsx

---
+++ src/components/academy/Dojo/AcademyDojo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AcademyDojo: React.FC = () => {
+  return (
+    <div className="academy-dojo">
+      <h2>🏫 幽玄道場</h2>
+      <p>この機能は現在開発中です。</p>
+      <p>麻雀学習システムと弱点分析が含まれます。</p>
+    </div>
+  );
+};
